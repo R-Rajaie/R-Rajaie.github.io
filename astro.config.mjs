0f6096104eb41462666c03237302a879cc301067
R-Rajaie <ryan.rajaie04@gmail.com>
2025-09-09 02:27:28 -0700

    i hate comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://r-rajaie.github.io', // update when you know it
+  site: 'https://r-rajaie.github.io',
   output: 'static'
 });
